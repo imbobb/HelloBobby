e946f3a2f61d5d0511592262283c5e32268712c1
`B O B B Y™ <60647189+imbobb@users.noreply.github.com>
2020-12-22 14:22:09 +0700

    Add files via upload

--- sb.py
+++ sb.py
@@ -39,7 +39,7 @@ parser.add_argument('-T', '--traceback', type=str2bool, nargs='?', default=False
 parser.add_argument('-S', '--showqr', type=str2bool, nargs='?', default=False, metavar='', required=False, const=True, choices=[True, False], help='Show QR | Use : True/False')
 args = parser.parse_args()
 listAppType = ['DESKTOPWIN', 'DESKTOPMAC', 'IOSIPAD', 'CHROMEOS']
-line = LINE("farranrafael9@gmail.com","Massada_12345") #Input Your Email and Password Here
+line = LINE("EMAIL","PW") #Input Your Email and Password Here
 #=======================================================================================================================
 myMid = line.profile.mid
 programStart = time.time()
